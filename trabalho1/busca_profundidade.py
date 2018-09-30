425bd827419b829e1a9fdfbc808bb3b24f01cbad
Daniel Deda <37677288+NinjaDoMato@users.noreply.github.com>
2018-09-30 10:00:01 -0300

    Atualização na busca em profundidade

--- trabalho1/busca_profundidade.py
+++ trabalho1/busca_profundidade.py
@@ -1,29 +1,43 @@
 #!/usr/bin/env python
 
-from torre_hanoi import TorreHanoi
+from pprint import pprint
+from typing import List
+
+from problema import Problema
 
 
-class BuscaEmProfundidade():
-
-    def busca_profundidade(self, problema: ProblemaTorreHanoi):
-
+class BuscaEmProfundidade(object):
+    
+    def busca_profundidade(self, problema: Problema):        
         borda = [problema.estado_inicial]
-
+        memoria = [problema.estado_inicial]
+        
         while True:
-
+            
             if not borda:
-
-                print('Falha ao encontrar solução')
+                print('Falha ao encontrar solucao')
                 return []
-
-            estado = borda.pop(0)
-
-            print(estado)
-            print(type(estado))
-
+            
+            estado = borda.pop(-1)
+            
+            print(f'> Estado sendo avaliado:')
+            print(f'{estado}')
+            
             if problema.funcao_objetivo(estado):
-
-                print('Solução encontrada!')
+                print('\n >>>> Solucao encontrada <<<< \n')
                 return problema.solucao(estado)
-
-            borda.append(problema.funcao_sucessora(estado))
+            
+            sucessores = problema.funcao_sucessora(estado)
+            borda.extend([x for x in sucessores if x not in memoria])
+            memoria.extend([x for x in sucessores if x not in memoria])
+            
+            for x in sucessores:
+                print(x)
+                
+            for x in memoria:
+                print(x)
+                
+            print()
+            
+            memoria.extend(sucessores)
+            print(f'> Estados sucessores: {len(sucessores)}')
